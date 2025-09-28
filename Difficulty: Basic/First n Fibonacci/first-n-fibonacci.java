9c314e6db01dc9728ac2413a773eb7e84bae6db4
Piyush Tiwari <piyusht122005@gmail.com>
2025-09-28 21:45:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
+++ Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
@@ -1,16 +1,24 @@
 // User function Template for Java
 
 class Solution {
-    public static int helper(int n){
-        if(n<=1) return n;
-        
-        return helper(n-1)+helper(n-2);
-    }
     public static int[] fibonacciNumbers(int n) {
         int[] ans=new int[n];
         
-        for(int i=0;i<n;i++){
-            ans[i]=helper(i);
+        ans[0]=0;
+        if(n==1) return ans;
+
+        ans[1]=1;
+        if(n==2) return ans;
+            
+        int a=ans[0];
+        int b=ans[1];
+            
+        for(int i=2;i<n;i++){
+            int sum=a+b;
+            a=b;
+            b=sum;
+            
+            ans[i]=sum;
         }
         return  ans;
     }
