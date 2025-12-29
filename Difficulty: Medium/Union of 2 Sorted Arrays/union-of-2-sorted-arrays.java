0bf3e366ce3bed2a089eca1c9fc1cb4d7542d396
Piyush Tiwari <piyusht122005@gmail.com>
2025-12-29 12:14:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,19 +1,25 @@
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
-        HashSet<Integer> set=new HashSet<>();
-        
-        for(int ele:a){
-            set.add(ele);
-        }
-        
-        for(int ele:b){
-            set.add(ele);
-        }
-        
         ArrayList<Integer> arr = new ArrayList<>();
-        for(int ele:set) arr.add(ele);
+        for(int ele:a){
+            if(arr.size() == 0 || arr.get(arr.size()-1)!=ele) arr.add(ele);
+        }
         
-        Collections.sort(arr);
+        int i = 0;
+        int j = 0;
+        while(i<arr.size() && j<b.length){
+            if(i>0 && arr.get(i-1)== b[j]) j++;
+            else if (arr.get(i) > b[j]){
+                arr.add(i,b[j++]);
+            }
+            else i++;
+        }
+        
+        while(j<b.length){
+            if(arr.get(arr.size()-1)!= b[j]) arr.add(b[j++]);
+            else j++;
+        }
+        
         return arr;
     }
 }
