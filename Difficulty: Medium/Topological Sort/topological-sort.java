2499d7a73d38c0b041516b23f89ba7af10562d39
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-02 21:27:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,14 +1,24 @@
 class Solution {
-    public void dfs(int i,boolean[] vis,List<List<Integer>> adj,ArrayList<Integer> ans){
+    public void bfs(int i,boolean[] vis,List<List<Integer>> adj,int[] In,ArrayList<Integer> ans){
         vis[i] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
         
-        for(int ele:adj.get(i)){
-            if(!vis[ele]){
-                dfs(ele,vis,adj,ans);
+        while(!q.isEmpty()){
+            int temp = q.remove();
+
+            for(int ele:adj.get(temp)){
+                if(In[ele] > 0) {
+                    In[ele]--;
+                }
+                if(!vis[ele] && In[ele]==0){
+                    q.add(ele);
+                    vis[ele] = true;
+                }
             }
+            ans.add(temp);
         }
         
-        ans.add(i);
     }
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         ArrayList<Integer> ans = new ArrayList<>();
@@ -16,21 +26,22 @@ class Solution {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<V;i++) adj.add(new ArrayList<>());
         
+        int[] indegree = new int[V];
         for(int[] arr:edges){
             int src = arr[0];
             int dest = arr[1];
             
+            indegree[dest]++;
             adj.get(src).add(dest);
         }
         
         boolean[] vis = new boolean[V];
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj,ans);
+            if(!vis[i] && indegree[i]==0){
+                bfs(i,vis,adj,indegree,ans);
             }
         }
         
-        Collections.reverse(ans);
         return ans;
     }
 }
