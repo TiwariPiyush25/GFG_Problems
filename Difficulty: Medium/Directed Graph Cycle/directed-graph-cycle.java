f49747db4de105ff3ea0fb72fbf7284d23b318bf
Piyush Tiwari <piyusht122005@gmail.com>
2026-01-03 09:43:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void bfs(int i,boolean[] vis,List<List<Integer>> adj,int[] In,ArrayList<Integer> ans){
+        vis[i] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        
+        while(!q.isEmpty()){
+            int temp = q.remove();
+
+            for(int ele:adj.get(temp)){
+                if(In[ele] > 0) {
+                    In[ele]--;
+                }
+                if(!vis[ele] && In[ele]==0){
+                    q.add(ele);
+                    vis[ele] = true;
+                }
+            }
+            ans.add(temp);
+        }
+        
+    }
+    public boolean isCyclic(int V, int[][] edges) {
+       ArrayList<Integer> ans = new ArrayList<>();
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
+        
+        int[] indegree = new int[V];
+        for(int[] arr:edges){
+            int src = arr[0];
+            int dest = arr[1];
+            
+            indegree[dest]++;
+            adj.get(src).add(dest);
+        }
+        
+        boolean[] vis = new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!vis[i] && indegree[i]==0){
+                bfs(i,vis,adj,indegree,ans);
+            }
+        }
+        
+        return ans.size() != V;
+    }
+}
