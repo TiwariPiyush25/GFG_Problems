dc5899093c812829bee89d5392e01672c90f6fcc
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-10 21:33:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,4 +1,5 @@
 class Solution {
+    public boolean flag;
     public class pair{
         int node;
         int parent;
@@ -8,50 +9,43 @@ class Solution {
             this.parent = p;
         }
     }
-    public void bfs(int i,List<List<Integer>> adj,boolean[] vis,boolean[] flag){
+    public void bfs(int i,List<List<Integer>> adj,boolean[] vis){
         vis[i] = true;
         Queue<pair> q = new LinkedList<>();
-        
         q.add(new pair(i,-1));
+        
         while(!q.isEmpty()){
-            pair p = q.remove();
-            int N = p.node;
-            int P = p.parent;
+            pair top = q.remove();
             
-            for(int ele:adj.get(N)){
-                if(!vis[ele] && ele != P){
-                    q.add(new pair(ele,N));
+            for(int ele : adj.get(top.node)){
+                if(!vis[ele] && ele!=top.parent){
+                    q.add(new pair(ele,top.node));
                     vis[ele] = true;
                 }
-                else if(vis[ele] && ele != P){
-                    flag[0] = true;
-                    return;
+                else if(vis[ele] && ele!=top.parent){
+                    flag = true;
                 }
             }
         }
-        
     }
     public boolean isCycle(int V, int[][] edges) {
+        flag = false;
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<V;i++) adj.add(new ArrayList<>());
         
-        for(int[] arr:edges){
-            int v1 = arr[0];
-            int v2 = arr[1];
+        for(int[] edge :edges){
+            int u = edge[0], v = edge[1];
             
-            adj.get(v1).add(v2);
-            adj.get(v2).add(v1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-    
         boolean[] vis = new boolean[V];
-        boolean[] flag = new boolean[1];
         for(int i=0;i<V;i++){
+            if(flag) break;
             if(!vis[i]){
-                bfs(i,adj,vis,flag);
-                if(flag[0]) return true;
+                bfs(i,adj,vis);
             }
         }
-        
-        return false;
+        return flag;
     }
 }
