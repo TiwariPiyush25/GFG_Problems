68e846dfce06c5ae844a3775b0b4ac5a4469e9b3
Piyush Tiwari <piyusht122005@gmail.com>
2026-03-11 09:49:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,47 +1,37 @@
 class Solution {
-    public void bfs(int i,boolean[] vis,List<List<Integer>> adj,int[] In,ArrayList<Integer> ans){
+    public void dfs(int i,List<List<Integer>> adj,boolean[] vis,ArrayList<Integer> ans){
         vis[i] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
         
-        while(!q.isEmpty()){
-            int temp = q.remove();
-
-            for(int ele:adj.get(temp)){
-                if(In[ele] > 0) {
-                    In[ele]--;
-                }
-                if(!vis[ele] && In[ele]==0){
-                    q.add(ele);
-                    vis[ele] = true;
-                }
+        
+        for(int ele:adj.get(i)){
+            if(!vis[ele]){
+                dfs(ele,adj,vis,ans);
             }
-            ans.add(temp);
         }
         
+        ans.add(i);
     }
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
-        
-        int[] indegree = new int[V];
-        for(int[] arr:edges){
-            int src = arr[0];
-            int dest = arr[1];
-            
-            indegree[dest]++;
-            adj.get(src).add(dest);
-        }
-        
-        boolean[] vis = new boolean[V];
-        for(int i=0;i<V;i++){
-            if(!vis[i] && indegree[i]==0){
-                bfs(i,vis,adj,indegree,ans);
-            }
-        }
-        
-        return ans;
+       ArrayList<Integer> ans = new ArrayList<>();
+       
+       List<List<Integer>> adj = new ArrayList<>();
+       for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
+       }
+       for(int[] edge : edges){
+           int u = edge[0], v = edge[1];
+           
+           adj.get(u).add(v);
+       }
+       
+       boolean[] vis = new boolean[V];
+       for(int i=0;i<V;i++){
+           if(!vis[i]){
+               dfs(i,adj,vis,ans);
+           }
+       }    
+       
+       Collections.reverse(ans);
+       return ans;
     }
 }
